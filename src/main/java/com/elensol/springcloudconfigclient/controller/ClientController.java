adf3ca3b7ecf970cfba0642dd797832f8bb6aecb
Didier <didorg@gmail.com>
2020-03-03 01:01:46 -0500

    III - Config: Store config in the environment

--- src/main/java/com/elensol/springcloudconfigclient/controller/ClientController.java
+++ src/main/java/com/elensol/springcloudconfigclient/controller/ClientController.java
@@ -1,11 +1,13 @@
 package com.elensol.springcloudconfigclient.controller;
 
 import com.elensol.springcloudconfigclient.config.Config;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping(value = "/client")
 public class ClientController {
 
