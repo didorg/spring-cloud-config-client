b5e476706f2eb90a4fbf4f9ee39f35cda09b2d6c
Didier <didorg@gmail.com>
2020-03-03 12:41:52 -0500

    update client

--- src/main/java/com/elensol/springcloudconfigclient/controller/ClientController.java
+++ src/main/java/com/elensol/springcloudconfigclient/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.elensol.springcloudconfigclient.controller;
 
 import com.elensol.springcloudconfigclient.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientController {
 
     private final Config config;
+    @Value("${my.message: Hey there! from default message}")
+    private String message;
 
+    @Autowired
     public ClientController(Config config) {
         this.config = config;
     }
 
+    // One way
     @GetMapping(value = "/greeting")
     public String greeting(){
         return this.config.getGreeting();
+    }
+
+    // Other way
+    @GetMapping(value = "/message")
+    public String message(){
+        return message;
     }
 }
