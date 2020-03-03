cd8dd4261c586ab357b37a2ed482dd5e3115889b
Didier <didorg@gmail.com>
2020-03-02 23:47:28 -0500

    update yml

--- src/main/java/com/elensol/springcloudconfigclient/config/Config.java
+++ src/main/java/com/elensol/springcloudconfigclient/config/Config.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "client")
+@ConfigurationProperties(prefix = "my")
 public class Config {
 
     private String greeting;
